4bedeb9a3433001bdc1b230c5faaaadadc6a6b3f
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-01-17 14:43:45 -0500

    chore(internal): link to issue on vitest about concurrent snapshot testing"

--- ember-apply/tests/cli.test.ts
+++ ember-apply/tests/cli.test.ts
@@ -5,6 +5,7 @@ import { apply, diff, diffSummary, newEmberApp } from '../src/test-utils';
 
 // let it = test.concurrent;
 // Snapshot testing is broken in concurrent tests
+// see: https://github.com/vitest-dev/vitest/issues/551
 let it = test;
 
 describe('CLI', () => {
