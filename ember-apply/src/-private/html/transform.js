5ae8ea4d08f12ae1afb63454982912f914a0909d
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-01-22 11:39:59 -0500

    fix(html, transform): move from rehype back to posthtml
    resolves: https://github.com/NullVoxPopuli/ember-apply/issues/37
    
    BREAKING CHANGE: turns out rehype isn't a good fit for codemods
    rehype's primary use case is generated code -> generated code.
    We need userland code -> userland code, so maintaining formatting is
    important.

--- ember-apply/src/-private/html/transform.js
+++ ember-apply/src/-private/html/transform.js
@@ -1,64 +1,53 @@
 // @ts-check
 
+import fs from 'fs/promises';
+
+import posthtml from 'posthtml';
+
 /**
  *
- * @typedef {import('unist').Node} Node
- * @typedef {import('unist').Parent} Parent
+ * @typedef {posthtml.Plugin<unknown>} Plugin
+ * @typedef {posthtml.Node | string} Node
  *
  * @typedef {object} AddHTMLOptions
  * @property {string} before which tag to insert text before
- *
- * @typedef {import('hast').Root} Root
- * @typedef {import('unified').Plugin<Array<void>, Root, Root>} Plugin
- *
- * @typedef {import('unified').CompilerFunction<Node, string>} CompilerFunction
  */
 
-import fs from 'fs/promises';
-import { unified } from 'unified';
-import rehypeParse from 'rehype-parse';
-import rehypeStringify from 'rehype-stringify';
-import { visit } from 'unist-util-visit';
-import rehypeRaw from 'rehype-raw';
-
 /**
- * Transform HTML with rehype
+ * Transform HTML
+ *
+ * Uses [posthtml](https://posthtml.org/#/) for transformation.
+ * See: [posthtml plugins](https://github.com/posthtml/posthtml-plugins/blob/master/src/pluginsNames.json) for examples.
  *
  * @example
  * the `before` option is to insert HTML before the first tag specified -- `link` in this case.
+ *
  * ```js
  * import { html } from 'ember-apply';
- * import { visit } from 'unist-util-visit';
  *
  * await html.transform(filePath, (tree) => {
- *   let found = false;
+ *   tree.match(({ tag: 'head' }, 'head') => {
+ *     if (node.content) {
+ *       node.content.unshift({ tag: 'noscript', content: 'Please enable javascript' });
+ *     }
  *
- *   visit(tree, (node, index, parent) => {
- *     let trailing = parent.children.splice(index);
- *
- *     // delete children nodes
- *     parent.children = [];
+ *     return node;
  *   });
  * });
  * ```
  *
+ * Example borrowed from posthtml-noscript
+ *
  * @param {string} filePath
- * @param {(tree: Node) => void} plugin
+ * @param {Plugin | Plugin[]} plugin
  */
 export async function transform(filePath, plugin) {
   let code = (await fs.readFile(filePath)).toString();
 
-  let transformed = await unified()
-    .use(rehypeParse, { emitParseErrors: true, duplicateAttribute: false })
-    .use(() => plugin)
-    .use(rehypeRaw, { passThrough: ['text', 'html'] })
-    .use(rehypeStringify, {
-      upperDoctype: true,
-      allowDangerousHtml: true,
-    })
-    .process(code);
+  let plugins = Array.isArray(plugin) ? plugin : [plugin];
+  let result = await posthtml(plugins).process(code);
 
-  await fs.writeFile(filePath, transformed.value);
+  await fs.writeFile(filePath, result.html);
 }
 
 /**
@@ -69,63 +58,61 @@ export async function transform(filePath, plugin) {
  * ```js
  * import { html } from 'ember-apply';
  *
- * await html.add(
- *   'app/index.html',
- *   `<link integrity="" rel="stylesheet" href="{{rootURL}}assets/tailwind.css">`,
- *   { before: 'link' }
- * );
+ * await html.inserText('app/index.html', {
+ *   text: `<link integrity="" rel="stylesheet" href="{{rootURL}}assets/tailwind.css">`,
+ *   beforeFirst: 'link',
+ * });
  * ```
  *
+ * @typedef {object} InsertBeforeFirstOptions
+ * @property {string} beforeFirst the HTML tag to insert `text` before
+ * @property {string} text the HTML to inject before the first tag matching `tag`
+ *
  * @param {string} filePath
- * @param {string} html the HTML to inject before the first tag matching `before`
- * @param {AddHTMLOptions} options
+ * @param {InsertBeforeFirstOptions} options
  */
-export async function add(filePath, html, { before = '' }) {
+export async function insertText(filePath, { beforeFirst, text }) {
   let code = (await fs.readFile(filePath)).toString();
 
-  if (code.includes(html)) {
+  if (code.includes(text)) {
     return;
   }
 
   /**
-   * @type {Node[]}
+   * @param {string} tag
+   * @param {string} text
    */
-  let targetAST = [];
+  function insertBeforeFirst(tag, text) {
+    /** @type {Plugin} */
+    return (tree) => {
+      let found = false;
+      /** @type {Node} */
+      let previousNode;
 
-  await unified()
-    .use(rehypeParse, { fragment: true })
-    .use(() => (tree) => {
-      targetAST = tree.children;
-    })
-    .use(
-      rehypeStringify,
+      // Types are wrong for Node. tag: false is how you say "array of any content"
       // @ts-expect-error
-      { fragment: true }
-    )
-    .process(html);
+      tree.walk((node) => {
+        if (!found && node.tag === tag) {
+          found = true;
 
-  await transform(filePath, (tree) => {
-    let found = false;
+          if (typeof previousNode === 'string') {
+            let parts = previousNode.split('\n');
+            let indentation = parts[parts.length - 1];
 
-    visit(
-      tree,
-      { type: 'element', tagName: before },
-      /**
-       * @param {Node} node
-       * @param {number} index
-       * @param {Parent} parent
-       */
-      (_node, index, parent) => {
-        if (found) {
-          return;
+            return { tag: false, content: [text, indentation, node], attrs: {} };
+          }
+
+          return { tag: false, content: [text, node], attrs: {} };
         }
 
-        found = true;
+        previousNode = node;
 
-        let trailing = parent.children.splice(index);
+        return node;
+      });
 
-        parent.children = [...parent.children, ...targetAST, ...trailing];
-      }
-    );
-  });
+      return tree;
+    };
+  }
+
+  return await transform(filePath, insertBeforeFirst(beforeFirst, text));
 }
