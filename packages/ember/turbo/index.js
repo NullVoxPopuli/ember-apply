d205b66fa7ef89820f524d3d2a4819e9f1fbfac2
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-06-10 11:26:38 -0400

    Linting madness

--- packages/ember/turbo/index.js
+++ packages/ember/turbo/index.js
@@ -7,6 +7,68 @@ import assert from "node:assert";
 // @ts-ignore
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const LINTS = {
+  prettier: {
+    scripts: {
+      "lint:prettier:fix": "prettier --write .",
+      "lint:prettier": "prettier --check .",
+    },
+    deps: ["prettier", "prettier-plugin-ember-template-tag"],
+  },
+  eslint: {
+    scripts: {
+      "lint:js": "eslint .",
+      "lint:js:fix": "pnpm lint:js --fix",
+    },
+    deps: [
+      "@nullvoxpopuli/eslint-configs",
+      "eslint",
+      "@babel/eslint-parser",
+      "eslint-plugin-ember",
+      "eslint-plugin-n",
+    ],
+  },
+  templates: {
+    scripts: {
+      "lint:hbs": "ember-template-lint . --no-error-on-unmatched-pattern",
+      "lint:hbs:fix": "pnpm lint:hbs --fix",
+    },
+    deps: ["ember-template-lint"],
+  },
+  css: {
+    scripts: {
+      "lint:css": 'stylelint "**/*.css"',
+      "lint:css:fix": "pnpm lint:css --fix",
+    },
+    deps: ["stylelint", "stylelint-config-standard"],
+  },
+  package: {
+    scripts: {
+      pack: "pnpm pack",
+      "lint:package": "publint",
+      "lint:published-types": "attw *.tgz || exit 0",
+    },
+    deps: ["@arethetypeswrong/cli", "publint"],
+  },
+  typescript: {
+    scripts: {
+      "lint:types": "tsc --noEmit",
+    },
+    deps: [
+      "@tsconfig/ember",
+      "typescript",
+      "@typescript-eslint/eslint-plugin",
+      "@typescript-eslint/parser",
+    ],
+  },
+  glint: {
+    scripts: {
+      "lint:types": "glint",
+    },
+    deps: ["@glint/core"],
+  },
+};
+
 export default async function run() {
   let pkgManager = await project.getPackageManager();
 
@@ -22,6 +84,9 @@ export default async function run() {
       // -> remove all lint scripts except lint and lint:fix
       //
       // each workspace:
+      // - update .prettierrc.cjs and .eslintrc.cjs
+      //    - support gjs, etc etc
+      //    - expose the primitives for each of the linting styles
       // - has eslint?
       //  -> add lint:js, lint:js:fix
       // - has prettier?
