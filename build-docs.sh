a9a00019bbbda48f0b2428bb0f907af7f45ad5f6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-12 16:48:42 -0400

    Update workfows again

--- build-docs.sh
+++ build-docs.sh
@@ -3,6 +3,11 @@
 # Dear cloudflare, if you see this,
 # if you support pnpm in your build workers, 
 # I can replace all of this with pnpm build:docs
+#
+# Also, due to taking over the `functions` path, you broke 
+# type doc deploys....
+# 
+# I would very much like to not have this script at all.
 echo "==================================================="
 echo "||            For running on CI  only            ||"
 echo "==================================================="
