27d33362c13c467fb8ed56070dc75f7f80e63ece
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-20 16:41:11 -0700

    Sidequest

--- ember-apply/src/-private/ember/index.js
+++ ember-apply/src/-private/ember/index.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 export * as deprecations from './deprecations.js';
+export * from './experiments.js';
 export * from './queries.js';
 export * from './templates.js';
 export * from './utils.js';
