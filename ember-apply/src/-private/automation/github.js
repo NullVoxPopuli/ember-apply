a67f2182bf28fda8821f0ea143436013fd51f2de
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-01 18:22:42 -0400

    GH progress

--- ember-apply/src/-private/automation/github.js
+++ ember-apply/src/-private/automation/github.js
@@ -1,2 +1,11 @@
 export function ensurePR() {
 }
+
+// https://docs.github.com/en/rest/orgs/members?apiVersion=2022-11-28#list-organization-members
+// use (and require) the gh cli
+/**
+  * @type {string} org
+  */
+export async function getOrgMembersForReview(org) {
+
+}
