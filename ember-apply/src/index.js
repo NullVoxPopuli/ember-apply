c04e4f438b077849e2a2b13ae35dfe6e39bd1394
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-08 11:30:39 -0400

    Fix bad import

--- ember-apply/src/index.js
+++ ember-apply/src/index.js
@@ -4,11 +4,11 @@
 // Each of these re-exported modules points at an index.js file which
 // defines the Public API for each namespace
 
+export * as automation from './-private/automation/index.js';
 export * as css from './-private/css/index.js';
 export * as ember from './-private/ember/index.js';
 export * as files from './-private/files/index.js';
 export * as html from './-private/html/index.js';
 export * as js from './-private/js/index.js';
 export * as packageJson from './-private/package-json/index.js';
-export * as automation from './-private/project/automation/index.js';
 export * as project from './-private/project/index.js';
