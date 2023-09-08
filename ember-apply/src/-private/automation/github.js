eabf2cff1cafde93133080c1f10f08ba7521ae10
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-08 11:32:59 -0400

    fix

--- ember-apply/src/-private/automation/github.js
+++ ember-apply/src/-private/automation/github.js
@@ -3,6 +3,8 @@ export function ensurePR() {}
 // https://docs.github.com/en/rest/orgs/members?apiVersion=2022-11-28#list-organization-members
 // use (and require) the gh cli
 /**
- * @type {string} org
+ * Not yet implemented
+ *
+ * @param {string} org
  */
 export async function getOrgMembersForReview(org) {}
