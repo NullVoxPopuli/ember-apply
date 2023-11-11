013bfe439929fd0846dca6c909e82a089916de82
Michal Bryxí <michal.bryxi@gmail.com>
2023-11-11 19:25:19 +0100

    fix: More resilient tailwind config
    - Make it so that it's cwd aware
    - Make it so that it's easy to extend for custom library includes

--- packages/ember/tailwind-webpack/files/config/tailwind.config.js
+++ packages/ember/tailwind-webpack/files/config/tailwind.config.js
@@ -1,6 +1,22 @@
+const path = require('path');
+
+const appRoot = path.join(__dirname, '../../');
+const libraries = [
+  /* someLibraryName */
+];
+
+const libraryPaths = libraries.map((name) =>
+  path.dirname(require.resolve(name)),
+);
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{gjs,gts,hbs,html,js,ts}'],
+  content: [
+    `${appRoot}/app/**/*.{js,ts,hbs,gjs,gts,html}`,
+    ...libraryPaths.map(
+      (libraryPath) => `${libraryPath}/**/*.{js,ts,hbs,gjs,gts,html}`,
+    ),
+  ],
   theme: {
     extend: {},
   },
