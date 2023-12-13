a5f70d6dcb628ac24e2f706361ab2e45d582314e
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-13 11:34:16 -0500

    Add unstable-embroider

---
+++ packages/ember/unstable-embroider/index.js
@@ -0,0 +1,62 @@
+// @ts-check
+import { packageJson, project } from 'ember-apply';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import latestVersion from 'latest-version';
+
+export default async function run() {
+  await useUnstableEmbroider();
+}
+
+const EMBROIDER_PACKAGES = [
+  '@embroider/addon-dev',
+  '@embroider/addon-shim',
+  '@embroider/core',
+  '@embroider/compat',
+  '@embroider/macros',
+  '@embroider/hbs-loader',
+  '@embroider/reverse-exports',
+  '@embroider/router',
+  '@embroider/shared-internals',
+  '@embroider/test-setup',
+  '@embroider/util',
+  '@embroider/vite',
+  '@embroider/webpack',
+];
+
+const DEP_TYPES = ['dependencies', 'devDependencies', 'peerDependencies'];
+
+async function useUnstableEmbroider() {
+  let withVersions = await Promise.all(
+    EMBROIDER_PACKAGES.map(async (name) => {
+      let version = await latestVersion(name, { version: 'unstable' });
+
+      return [name, version];
+    }),
+  );
+
+  // Update in-monorepo things (at the very least for busting turbo's cache)
+  // eslint-disable-next-line no-unused-vars
+  for await (let workspace of await project.eachWorkspace()) {
+    await packageJson.modify((json) => {
+      for (let depType of DEP_TYPES) {
+        for (let [name, version] of withVersions) {
+          if (json[depType]?.[name]) {
+            json[depType][name] = version;
+          }
+        }
+      }
+    });
+  }
+
+  // Handle transient dependencies
+  await packageJson.modify((json) => {
+    for (let [name, version] of withVersions) {
+      json.pnpm.overrides[name] = version;
+    }
+  });
+}
+// @ts-ignore
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+run.path = __dirname;
---
+++ packages/ember/unstable-embroider/index.test.ts
@@ -0,0 +1,23 @@
+import { apply, diff, diffSummary, newEmberApp } from 'ember-apply/test-utils';
+import { describe, expect, it } from 'vitest';
+
+import { default as embroider } from './index.js';
+
+describe('embroider', () => {
+  it('default export exists', () => {
+    expect(typeof embroider).toEqual('function');
+  });
+
+  describe('applying to an ember app', () => {
+    it('works via CLI', async () => {
+      let appLocation = await newEmberApp();
+
+      await apply(appLocation, embroider.path);
+
+      expect(await diffSummary(appLocation)).toMatchSnapshot();
+      expect(
+        await diff(appLocation, { ignoreVersions: true })
+      ).toMatchSnapshot();
+    });
+  });
+});
