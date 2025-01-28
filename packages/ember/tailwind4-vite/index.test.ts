4e0cbb1befa76335e301b6547ab6ff9fd24bddbf
Michal Bryxí <michal.bryxi@gmail.com>
2025-01-28 09:20:07 +0100

    feat: Fork tailwind-vite to tailwind4-vite

---
+++ packages/ember/tailwind4-vite/index.test.ts
@@ -0,0 +1,23 @@
+import { apply, diff, diffSummary, newEmberApp } from "ember-apply/test-utils";
+import { describe, expect, it } from "vitest";
+
+import { default as tailwindVite } from "./index.js";
+
+describe("tailwind4-vite", () => {
+  it("default export exists", () => {
+    expect(typeof tailwindVite).toEqual("function");
+  });
+
+  describe("applying to an ember app", () => {
+    it("works via CLI", async () => {
+      let appLocation = await newEmberApp();
+
+      await apply(appLocation, tailwindVite.path);
+
+      expect(await diffSummary(appLocation)).toMatchSnapshot();
+      expect(
+        await diff(appLocation, { ignoreVersions: true })
+      ).toMatchSnapshot();
+    });
+  });
+});
