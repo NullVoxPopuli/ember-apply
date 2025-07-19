216e3f063efd4b413725ac19e784b957d7ac208d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-19 12:14:25 -0400

    wrangler

--- build-docs.sh
+++ build-docs.sh
@@ -19,34 +19,5 @@ pnpm build:docs
 
 cd packages/docs/
 
-# Can I just make "functions" work?
-echo '{ "route": { "/*": "/*" } }' >> dist/_routes.json
-echo '{ "name": "ember-apply",  "compatibility_date": "2025-07-19", "assets": { "directory": "." } }' >> dist/wrangler.json
+cp wrangler.json ./dist/
 
-# # cloudflare broke all user's Sites that have a "functions" path.
-# # ... thanks cloudflare. you're usually such a good company :p 
-# #
-# #  - https://github.com/NullVoxPopuli/ember-resources/issues/679
-# #  - https://github.com/TypeStrong/typedoc/issues/2111
-# #  - https://github.com/cloudflare/wrangler2/issues/2240
-# #  - https://community.cloudflare.com/t/functions-dir-is-ignored-in-deploy/438211
-# DOCS_DIRECTORY='dist'
-# FUNCTIONS='functions'
-# RENAMED_FUNCTIONS='funcs'
-# FUNCTIONS_DIRECTORY="$DOCS_DIRECTORY/$FUNCTIONS"
-# RENAMED_FUNCTIONS_DIRECTORY="$DOCS_DIRECTORY/$RENAMED_FUNCTIONS"
-#
-# mv $FUNCTIONS_DIRECTORY $RENAMED_FUNCTIONS_DIRECTORY
-#
-# echo $PWD
-# echo $DOCS_DIRECTORY
-# find $DOCS_DIRECTORY \
-#   -type f \
-#   -name "*.html" \
-#   -exec \
-#     sed -i -e "s/${FUNCTIONS}/${RENAMED_FUNCTIONS}/g" {} +
-#
-# echo "Renamed $FUNCTIONS_DIRECTORY to $RENAMED_FUNCTIONS_DIRECTORY"
-#
-#
-#
